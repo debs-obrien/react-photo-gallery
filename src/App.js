d5c651e79a5d46c880b96d9a74cfbed88ad91b5c
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-11-04 08:49:19 +0100

    first changes to app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header >
+
+        </header>
+
+            <h1>Results</h1>
+          <h3>No Results Found</h3>
+          <p>
+              Your search did not return any results. Please try again.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
